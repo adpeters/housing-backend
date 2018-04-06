081f58ab800bbbd28cf06a44917cb8e327ea96e8
Alec Peters <apeters@gatewaytocollege.org>
2018-04-05 21:45:24 -0700

    removed gunicorn command from entrypoint since we only want it to run when it is the command, updated entrypoint to run inputted command at end, and uncommented django migrations running in entrypoint

--- backend/bin/docker-entrypoint.sh
+++ backend/bin/docker-entrypoint.sh
@@ -6,11 +6,13 @@ echo  Running docker-entrypoint.sh...
 ./bin/getconfig.sh
 
 # Disabled migrations, too slow in the build process
-#python manage.py makemigrations --no-input
-#python manage.py migrate --no-input
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
 # Fire up a lightweight frontend to host the Django endpoints - gunicorn was the default choice
 # gevent used to address ELB/gunicorn issue here https://github.com/benoitc/gunicorn/issues/1194
 #gunicorn backend.wsgi:application -b :8000 --worker-class 'gevent' --workers 3 --keep-alive 60
-gunicorn backend.wsgi:application -b :8000 --worker-class 'gevent' --workers 1
+#gunicorn backend.wsgi:application -b :8000 --worker-class 'gevent' --workers 1
+
+exec "$@"
